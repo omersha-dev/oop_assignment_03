ffc28436abdac8e5ff7b8d2edcaf067139229fe8
Lior Turgeman <liortu@my.hit.ac.il>
2020-01-17 11:50:19 +0200

    Impplement Intrval

---
+++ oop_assignment_03/Interval.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Interval.h"
+
+using namespace std;
+
+
+template <class T>
+class  Interval
+{
+	Interval::Interval<T>(T first, T second)
+	{
+		
+	};
+	void Interval::setLow(T first)
+	{
+		
+	};
+	void Interval::setHigh(T Second)
+	{
+	
+	};
+
+	T Interval::getLow() const {return;};
+	T Interval::getHigh() const {return };
+
+	Interval::~Interval()
+	{
+	}
+};
--- oop_assignment_03/Interval.h
+++ oop_assignment_03/Interval.h
@@ -1,1 +1,20 @@
 #pragma once
+
+template <class T>
+
+class Interval
+{
+private:
+	T low;
+	T high;
+
+public:
+	Interval<T>(T first, T second);
+	void setLow(T first);
+	void setHigh(T Second);
+
+	T getLow() const;
+	T getHigh() const;
+};
+
+
