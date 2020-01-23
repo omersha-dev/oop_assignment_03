c7573ca676b18e23944d9983670e79f9291801dd
Omer Sha'ashua <omersha.dev@gmail.com>
2020-01-23 21:24:02 +0200

    Works - Need to validate against the input \ output in the instructions.

--- oop_assignment_03/Interval.h
+++ oop_assignment_03/Interval.h
@@ -1,8 +1,5 @@
 #pragma once
-/*
-דיסטרקטור
-ולהריץ הכל
-*/
+#include <iomanip>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -15,158 +12,231 @@ private:
 	T high;
 
 public:
-	Interval<T>() {
+	Interval() {
 		this->setLow(0);
 		this->setHigh(0);
 	}
-	Interval<T>(T first, T second) {
+	Interval(T first, T second) {
 		this->setLow(first);
 		this->setHigh(second);
 	}
-	void setLow(T first);
-	void setHigh(T Second);
-	T getLow() const;
-	T getHigh() const;
 
-	bool isEmpty() const;
-	bool isBefore(const Interval<T> &other)const;
-	bool isAfter(const Interval<T> &other)const;
-	bool intersects(const Interval<T> &other)const;
-	bool contains(const T &other)const;
+	void setLow(T first) { this->low = first; }
+	void setHigh(T second) { this->high = second; }
+	T getLow() const { return low; }
+	T getHigh() const { return high; }
 
-	bool checkInter(const Interval<T> other) const;
-	Interval<T>* operator && (const Interval<T> &other);
-	Interval<T>* operator || (const Interval<T> &other);
+	bool isEmpty() const
+	{
+		if (this->low == this->high)
+			return true;
+	}
 
-	friend std::ostream& operator<<(std::ostream& output, const Interval<T>& other);
+	bool isBefore(const Interval<T> &other)const
+	{
+		if (this->high < other.getLow())
+			return true;
+		return false;
+	}
 
-	~Interval();
+	bool isAfter(const Interval<T> &other)const
+	{
+		if (this->getLow() > other.getHigh())
+			return true;
+		return false;
+	}
+	bool intersects(const Interval<T> &other)const
+	{
+		if (this->checkInter(other) || other.checkInter(*this))	return true;
+
+		return false;
+	}
+	bool contains(const T &other)const
+	{
+		if ((this->getLow() < other) && (this->getHigh() > other))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool checkInter(const Interval<T> other) const
+	{
+		/*if (this->checkInter(other) || other.checkInter(*this))
+		{
+			return true;
+		}*/
+
+		return false;
+	}
+
+	Interval<T>* operator && (const Interval<T> &other)
+	{
+		Interval<T> *summery = new Interval<T>;
+		T min, max;
+		if (this->intersects(other))
+		{
+			if (this->getLow() > other.getLow())
+				min = this->getLow();
+			else
+				min = other.getLow();
+			if (this->getHigh() < other.getHigh())
+				max = this->getHigh();
+			else
+				max = other.getHigh();
+			summery->setLow(min);
+			summery->setHigh(max);
+			return summery;
+		}
+		return summery;
+	}
+	Interval<T> operator || (const Interval<T> other)
+	{
+		return other;
+	}
+
+	friend ostream& operator<<(ostream& output, const Interval<T> other)
+	{
+		string str = "";
+		if (other.getLow() == 0 && other.getHigh() == 0)
+		{
+			str = "Invalid interval";
+		}
+		else
+		{
+			str = "(" + to_string(other.getLow()) + "," + to_string(other.getHigh()) + ")";
+		}
+		output << str;
+		return output;
+	}
+
+	~Interval() {}
 };
 
-template<class T>
-void Interval<T>::setLow(T first) { this->low = first; }
-template<class T>
-void Interval<T>::setHigh(T second) { this->high = second; }
-
-template<class T>
-T Interval<T>::getLow() const { return low; }
-template<class T>
-T Interval<T>::getHigh() const { return high; }
-
-template<class T>
-bool Interval<T>::isEmpty() const
-{
-	if (this->low == this->high)
-		return true;
-}
-
-template<class T>
-inline bool Interval<T>::isBefore(const Interval<T> &other) const
-{
-	if (this->high < other.getLow())
-		return true;
-	return false;
-}
-
-template<class T>
-inline bool Interval<T>::isAfter(const Interval<T> &other) const
-{
-	if (this->getLow() > other.getHigh())
-		return true;
-	return false;
-}
-
-template<class T>
-inline bool Interval<T>::intersects(const Interval<T> &other) const
-{
-	if (this->checkInter(other) || other.checkInter(*this))	return true;
-
-	return false;
-}
-
-template<class T>
-inline bool Interval<T>::contains(const T &other) const
-{
-	if ((this->getLow() < other) && (this->getHigh() > other))
-	{
-		return true;
-	}
-	return false;
-}
-
-template<class T>
-inline Interval<T>* Interval<T>::operator&&(const Interval<T>& other)
-{
-	Interval<T> *summery = new Interval<T>;
-	T min, max;
-	if (this->intersects(other))
-	{
-		if (this->getLow() > other.getLow())
-			min = this->getLow();
-		else
-			min = other.getLow();
-		if (this->getHigh() < other.getHigh())
-			max = this->getHigh();
-		else
-			max = other.getHigh();
-		summery->setLow(min);
-		summery->setHigh(max);
-		return summery;
-	}
-	return summery;
-}
-
-template<class T>
-inline Interval<T>* Interval<T>::operator||(const Interval<T>& other)
-{
-	Interval<T> *summery = new Interval<T>;
-	T min, max;
-	if (!this->intersects(other))
-	{
-		if (this->getLow() > other.getLow())
-			min = other.getLow();
-		else
-			min = this->getLow();
-		if (this->getHigh() < other.getHigh())
-			max = other.getHigh();
-		else
-			max = this->getHigh();
-		summery->setLow(min);
-		summery->setHigh(max);
-		return summery;
-	}
-	return summery;
-}
-
-template<class T>
-inline bool Interval<T>::checkInter(const Interval<T> other) const
-{
-	if (this->isAfter(other) || this->isBefore(other)) { return false; }
-
-	if (this->getLow() <= other.getLow() && this->getHigh() <= other.getHigh()) { return true; }
-
-	if (this->getLow() <= other.getLow() && this->getHigh() <= other.getHigh()) { return true; }
-
-	if (this->getLow() >= other.getLow() && this->getHigh() >= other.getHigh()) { return true; }
-
-	return false;
-}
-
-template<class T>
-inline std::ostream& operator<<(std::ostream& output, const Interval<T>& other)
-{
-	string str = "";
-	if (other.getLow() == 0 && other.getHigh() == 0)
-	{
-		str = "Invalid interval";
-	}
-	else
-	{
-		str = "(" + other.getLow() + "," + other.getHigh() + ")";
-	}
-	output << str;
-	return output;
-}
-
-template<class T>
-inline Interval<T>::~Interval() {}
+//template<class T>
+//void Interval<T>::setLow(T first) { this->low = first; }
+//template<class T>
+//void Interval<T>::setHigh(T second) { this->high = second; }
+//
+//template<class T>
+//T Interval<T>::getLow() const { return low; }
+//template<class T>
+//T Interval<T>::getHigh() const { return high; }
+//
+//template<class T>
+//bool Interval<T>::isEmpty() const
+//{
+//	if (this->low == this->high)
+//		return true;
+//}
+//
+//template<class T>
+//inline bool Interval<T>::isBefore(const Interval<T> &other) const
+//{
+//	if (this->high < other.getLow())
+//		return true;
+//	return false;
+//}
+//
+//template<class T>
+//inline bool Interval<T>::isAfter(const Interval<T> &other) const
+//{
+//	if (this->getLow() > other.getHigh())
+//		return true;
+//	return false;
+//}
+//
+//template<class T>
+//inline bool Interval<T>::intersects(const Interval<T> &other) const
+//{
+//	if (this->checkInter(other) || other.checkInter(*this))	return true;
+//
+//	return false;
+//}
+//
+//template<class T>
+//inline bool Interval<T>::contains(const T &other) const
+//{
+//	if ((this->getLow() < other) && (this->getHigh() > other))
+//	{
+//		return true;
+//	}
+//	return false;
+//}
+//
+//template<class T>
+//inline Interval<T>* Interval<T>::operator&&(const Interval<T>& other)
+//{
+//	Interval<T> *summery = new Interval<T>;
+//	T min, max;
+//	if (this->intersects(other))
+//	{
+//		if (this->getLow() > other.getLow())
+//			min = this->getLow();
+//		else
+//			min = other.getLow();
+//		if (this->getHigh() < other.getHigh())
+//			max = this->getHigh();
+//		else
+//			max = other.getHigh();
+//		summery->setLow(min);
+//		summery->setHigh(max);
+//		return summery;
+//	}
+//	return summery;
+//}
+//
+//template<class T>
+//inline Interval<T>* Interval<T>::operator||(const Interval<T>& other)
+//{
+//	Interval<T> *summery = new Interval<T>;
+//	T min, max;
+//	if (!this->intersects(other))
+//	{
+//		if (this->getLow() > other.getLow())
+//			min = other.getLow();
+//		else
+//			min = this->getLow();
+//		if (this->getHigh() < other.getHigh())
+//			max = other.getHigh();
+//		else
+//			max = this->getHigh();
+//		summery->setLow(min);
+//		summery->setHigh(max);
+//		return summery;
+//	}
+//	return summery;
+//}
+//
+//template<class T>
+//inline bool Interval<T>::checkInter(const Interval<T> other) const
+//{
+//	if (this->isAfter(other) || this->isBefore(other)) { return false; }
+//
+//	if (this->getLow() <= other.getLow() && this->getHigh() <= other.getHigh()) { return true; }
+//
+//	if (this->getLow() <= other.getLow() && this->getHigh() <= other.getHigh()) { return true; }
+//
+//	if (this->getLow() >= other.getLow() && this->getHigh() >= other.getHigh()) { return true; }
+//
+//	return false;
+//}
+// 
+//template<class T>
+//inline std::ostream& operator<<(std::ostream& output, const Interval<T>& other)
+//{
+//	string str = "";
+//	if (other.getLow() == 0 && other.getHigh() == 0)
+//	{
+//		str = "Invalid interval";
+//	}
+//	else
+//	{
+//		str = "(" + other.getLow() + "," + other.getHigh() + ")";
+//	}
+//	output << str;
+//	return output;
+//}
+//
+//template<class T>
+//inline Interval<T>::~Interval() {}
